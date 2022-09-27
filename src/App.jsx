46ee2b648fabedc4bd8842b049d843d15c8b7ac8
Brayan <tronks_465@hotmail.com>
2022-09-26 19:49:06 -0500

    NUBE

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import WeatherCard from './components/WeatherCard';
+import axios from 'axios';
+import Loading from './components/Loading';
+
+function App() {
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temperature, setTemperature] = useState();
+
+  useEffect(() => {
+    const success = (pos) => {
+
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+
+      setCoords(obj);
+
+    }
+
+    navigator.geolocation.getCurrentPosition(success)
+  }, [])
+
+  useEffect(() => {
+
+    if(coords){
+      const APIKEY = '21dc52a1b88f8eb7df88daaa7acd1022'
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
+      axios.get(URL)
+      .then(res => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const farenheit = (celsius * 9 / 5 + 32).toFixed(1);
+          setTemperature({celsius, farenheit}); 
+          setWeather(res.data)}
+        )
+      .catch(err => console.log(err))
+
+    }
+  }, [coords])
+
+  return (
+    <div className="App">
+      {
+        weather ? 
+          <WeatherCard weather={weather} temperature={temperature}/>
+          :
+          <Loading />
+      }
+    </div>
+  )
+}
+
+export default App
